28c428dacf6b7f82547f238efcc74b330818d3fb
oldbelom <oldbelom@gmail.com>
2022-01-15 13:38:42 +0300

    test4

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
-import React, { useContext } from "react";
-import { BrowserRouter } from "react-router-dom";
-import { useAuthState } from "react-firebase-hooks/auth";
-import Header from "./components/Header/Header";
-import AppRouter from "./components/AppRouter";
-import { Context } from "./index";
-import Loader from "./components/Loader/Loader";
+import React, { useContext } from 'react';
+import { HashRouter } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import Header from './components/Header/Header';
+import AppRouter from './components/AppRouter';
+import { Context } from './index';
+import Loader from './components/Loader/Loader';
 
 const App = () => {
-  const { auth } = useContext(Context);
-  const [, loading] = useAuthState(auth);
+    const { auth } = useContext(Context);
+    const [, loading] = useAuthState(auth);
 
-  if (loading) {
+    if (loading) {
+        return (
+            <div className="absolute-center">
+                <Loader />
+            </div>
+        );
+    }
+
     return (
-      <div className="absolute-center">
-        <Loader />
-      </div>
+        <HashRouter>
+            <Header />
+            <AppRouter />
+        </HashRouter>
     );
-  }
-
-  return (
-    <BrowserRouter>
-      <Header />
-      <AppRouter />
-    </BrowserRouter>
-  );
 };
 
 export default App;
